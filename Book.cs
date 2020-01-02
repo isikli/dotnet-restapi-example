a2ff5a0e60ae34ca21a07d60aeb9614bf9fb81cf
isikli <isiklia@gmail.com>
2020-01-02 18:57:43 +1100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace myrestapi.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+
+        public static implicit operator Book(EntityEntry<Book> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
